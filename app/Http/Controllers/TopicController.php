0e1f5b086411ea312b3d94c7c4b7a39c19ef32de
ahmedabderrafie <ahmedabderrafie@gmail.com>
2022-06-09 14:42:03 +0100

    First Init

---
+++ app/Http/Controllers/TopicController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+class TopicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return 'Index Topic';
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return 'Create Topic';
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return 'Store Topic';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Topic $topic)
+    {
+        //
+        return "My name: ".$topic->name;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Topic $topic)
+    {
+        return "I'm Edit";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Topic $topic)
+    {
+        //
+    }
+}
